761daaf53a2aa3f761b32e37a70dcc824398fd9f
wymeric <wymeric89@gmail.com>
2017-10-19 17:37:39 +0800

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser'; 
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Gaharu Gading Agrofarm';
+
+  constructor(private titleSrv:Title){}
+
+  ngOnInit(){
+    this.titleSrv.setTitle(this.title);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { 
+  MatToolbarModule,MatIconModule,MatButtonModule,MatMenuModule 
+} from '@angular/material';
 
+/*Components import */
 import { AppComponent } from './app.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,MenuComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,MatIconModule,MatButtonModule,MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
